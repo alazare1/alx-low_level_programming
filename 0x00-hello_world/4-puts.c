36cf306d49c6ad0d379ce3279e977dc2d0ef1b38
alazare1 <agefuye@gmail.com>
2023-03-19 04:29:35 -0700

    writing comments for main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /*
-The main function oversees all the code.
-
-The program will exit when it returns zero.
-
+*The main function oversees all the code.
+*
+*The program will exit when it returns zero.
+*
 */
 
 
 int main(void)
 {
-
+/* This is the main function */
 char char1[] = "\"Programming is like building a multilingual puzzle";
 
 puts(char1);
