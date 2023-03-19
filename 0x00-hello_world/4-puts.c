925b861ced4a758ee00d4098e5045bacdfc46071
alazare1 <agefuye@gmail.com>
2023-03-19 04:47:46 -0700

    commenting errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,17 +6,12 @@
  *
  * The main() function initializes a character array with a message, then
  * calls the puts() function to print the message to the console.
- * @char is used to initalize the char1 variable
+ *
  * @return 0 if the program runs successfully
  */
-# This is where we describe the main fuction
 int main(void)
 {
-
-char char1[] = "\"Programming is like building a multilingual puzzle";
-
-puts(char1);
-
-return (0);
-
+  char char1[] = "\"Programming is like building a multilingual puzzle";
+  puts(char1);
+  return 0;
 }
