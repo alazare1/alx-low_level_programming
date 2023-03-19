c3e0b0fffb451ab81d97fcba87bf045c1e772474
alazare1 <agefuye@gmail.com>
2023-03-19 06:09:54 -0700

    first iteration printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *printf - function to print formatted output
+ *
+ *return - returns value 0 which is success
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
