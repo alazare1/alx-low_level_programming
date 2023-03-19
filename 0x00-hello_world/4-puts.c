71acc04cea3ea3a2bcfb98e19bf26b22ff9f2f1d
alazare1 <agefuye@gmail.com>
2023-03-19 05:32:59 -0700

    quick fixes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,15 +3,15 @@
 /**
  * main - entry point it will be success when the return value is zero *
  * puts - Function to call char variable
- *
- * @return - if it returns zero it a success
- * 
+ * Returns: 0 if the program runs successfully, otherwise a non-zero value to indicate an error. 
  */ 
-int main(void)
+int main (void)
 {
-    char char1[] = "\"Programming is like building a multilingual puzzle";
 
-    puts(char1);
+char char1[] = "\"Programming is like building a multilingual puzzle";
 
-    return (0);
+puts(char1);
+
+return (0);
+
 }
