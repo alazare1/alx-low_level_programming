e31058e67cd7fa446aa6b8071259d24cb638d074
alazare1 <agefuye@gmail.com>
2023-03-19 04:21:14 -0700

    fix coding errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,10 @@
 int main(void)
 {
 
-char char1[] ="\"Programming is like building a multilingual puzzle";
+char char1[] = "\"Programming is like building a multilingual puzzle";
 
 puts(char1);
 
-return(0);
+return (0);
 
 }
