0447e03864a99d414b78fe2b8470390f80fd13f9
alazare1 <agefuye@gmail.com>
2023-03-19 04:52:47 -0700

    fixed spacing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,11 +7,13 @@
  * The main() function initializes a character array with a message, then
  * calls the puts() function to print the message to the console.
  *
- * @return 0 if the program runs successfully
+ * Return: 0 if the program runs successfully
  */
 int main(void)
 {
-  char char1[] = "\"Programming is like building a multilingual puzzle";
-  puts(char1);
-  return 0;
+	char char1[] = "\"Programming is like building a multilingual puzzle";
+	
+	puts(char1);
+
+	return (0);
 }
