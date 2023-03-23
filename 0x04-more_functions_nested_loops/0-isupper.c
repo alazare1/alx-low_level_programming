abadd602cc7d2799d01715c891849bb3f2d87482
alazare1 <agefuye@gmail.com>
2023-03-23 07:07:51 -0700

    check error

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,10 +5,19 @@
  * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
-int _isupper(int c)
+
+int main(void)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}	
+        char c = 'A';
+
+        if (_isupper(c))
+        {
+                return 1;
+        }
+        else {
+
+               return (0);
+
+        }
+
+}
