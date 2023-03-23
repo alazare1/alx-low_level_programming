54ede8335fa7636525d364d4523c71bd9b7ac901
alazare1 <agefuye@gmail.com>
2023-03-23 07:35:35 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,14 @@
-#include <_putchar.h> 
+#include "main.h"
+
+/**
+ * _putchar - prints characters
+ * @c - allows us to use character
+ * main - Entry point
+ * Return: returns 0 to exit
+*/
+int _putchar(char c)
+{
+    _putchar("_putchar");
+ 
+    return (0);
+}
