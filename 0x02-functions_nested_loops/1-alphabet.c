232cb209c7cf11f45a21e05c74e35db254de7c0e
alazare1 <agefuye@gmail.com>
2023-03-23 10:53:15 -0700

    print alphabet description

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  * @c - initalize at a and print character to z
- * print_alphabet - function to print alphabet
+ * @print_alphabet - function to print alphabet
  * Return: returns 0
  */
 void print_alphabet(void)
