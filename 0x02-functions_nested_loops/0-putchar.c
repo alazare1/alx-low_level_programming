0dc6aa4033ce351866345b8451c714cf2a97f0e4
alazare1 <agefuye@gmail.com>
2023-03-23 08:22:39 -0700

    made code betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,19 @@
  * @c - allows us to use character
  * main - Entry point
  * Return: returns 0 to exit
-*/
-int main (void)
+ */
+
+int main(void)
 {
-  	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
         _putchar('\n');
-    return (0);
+
+        return (0);
 }
