ff9a500304299efbcbd95738118c77099716e679
alazare1 <agefuye@gmail.com>
2023-03-23 06:06:29 -0700

    checking if c is uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+
+
+int _isupper(int c);
+
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{       
+        char c = 'A';
+
+	if (c>='A' && c<='Z')
+	{
+ 		return 1;
+	else {
+
+  	       return (0);
+}
