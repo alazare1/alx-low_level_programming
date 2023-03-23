30ab5e169ae547fc490cef202c6b9bcd0ea10ec0
alazare1 <agefuye@gmail.com>
2023-03-23 08:02:25 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,16 @@
  * main - Entry point
  * Return: returns 0 to exit
 */
-int _putchar(char c)
+int main (void)
 {
-    _putchar("_putchar");
- 
+  	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
     return (0);
 }
