1a366656fd08346cc5d2c35207b1eb0e18640cd8
alazare1 <agefuye@gmail.com>
2023-03-25 14:20:57 -0700

    program for checking int signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ *print_sign - function checks for sign of num
+ *@n: integer that is passed 
+ *Return: 1 if +, -1 if -, and 0
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+	return (1);
+	_putchar('+');
+}
+else if (n < 0)
+{
+	return (-1);
+	_putchar('-');
+}
+else
+{
+	return (0);
+	_putchar('0');
+}
+}
