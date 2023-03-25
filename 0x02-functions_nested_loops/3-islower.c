41850165e6db1486f3dcbf88b083d76639e89f64
alazare1 <agefuye@gmail.com>
2023-03-25 11:53:12 -0700

    first sub

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+/**
+ *_islower - returns True for lowercase, otherwise false
+ *@c - passed value through _islower function
+ *Return:1 if true, 0 if false
+ */
+
+int _islower(int c)
+{
+
+if (c >= 'a' && c <= 'z')
+{
+	return (1);
+}
+else
+{
+	return (0);
+}
+
+}
