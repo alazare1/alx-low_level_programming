3369758c7cf1d0b09c5ff61485b7822dbc341543
alazare1 <agefuye@gmail.com>
2023-03-27 07:32:53 -0700

    fixed spaces

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,31 +8,31 @@
 
 void puts_half(char *str)
 {
-    char *now = str;
-    int start_index;
-    int len;
+	char *now = str;
+	int start_index;
+	int len;
 
-    while (*now != '\n' && *now != 0)
-    {
-        now++;
-    }
+	while (*now != '\n' && *now != 0)
+	{
+		now++;
+	}
 
-    len = now - str;
-    start_index = len / 2;
+	len = now - str;
+	start_index = len / 2;
 
-    if (len % 2 == 1)
-    {
-        start_index++;
-    }
+	if (len % 2 == 1)
+	{
+		start_index++;
+	}
 
-    now = str + start_index;
+	now = str + start_index;
 
-    while (*now != '\n' && *now != 0)
-    {
-        _putchar(*now);
-        now++;
-    }
+	while (*now != '\n' && *now != 0)
+	{
+		_putchar(*now);
+		now++;
+	}
 
-    _putchar('\n');
+	_putchar('\n');
 }
 
