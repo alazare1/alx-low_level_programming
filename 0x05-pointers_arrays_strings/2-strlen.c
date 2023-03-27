d9ed955e675f13c0fad904820ccc3f7f95b9ceee
alazare1 <agefuye@gmail.com>
2023-03-27 01:48:04 -0700

    lenght of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+include "main.h"
+
+/*
+ * _strlen - find length of string
+ * @s: string
+ * return: value of p - s
+ */
+
+
+
+
+
+int _strlen(char *s)
+{
+	char *p = s;
+	while (*p)
+	{
+	p++;
+	}
+	return (p - s);
+}
