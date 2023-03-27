ec2713e23efa368951a6726016d81551fa72224d
alazare1 <agefuye@gmail.com>
2023-03-27 07:46:05 -0700

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,46 @@
+/*
+ * print_array - prints n elements of an integer array
+ *
+ * @a: pointer to the array of integers
+ * @n: number of elements to be printed
+ */
+void print_array(int *a, int n)
+{
+    int i = 0;
+    char digit;
+    int num;
+
+    while (i < n)
+    {
+        if (i > 0)
+        {
+            _putchar(',');
+            _putchar(' ');
+        }
+
+        num = a[i];
+        if (num == 0)
+        {
+            _putchar('0');
+            i++;
+            continue;
+        }
+
+        if (num < 0)
+        {
+            _putchar('-');
+            num *= -1;
+        }
+
+        while (num > 0)
+        {
+            digit = (num % 10) + '0';
+            _putchar(digit);
+            num /= 10;
+        }
+
+        i++;
+    }
+
+    _putchar('\n');
+}
