a1ded06c9887db33e2c927248307184fef4ae39d
alazare1 <agefuye@gmail.com>
2023-04-01 16:05:41 -0700

    print low without q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z' && c != 'q' && c != 'e')
+	{
+		putchar(c);
+		c++;
+	}
+
+putchar('\n');
+return (0);
+}
