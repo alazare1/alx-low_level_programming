02f09eafb79c6e02e13188b6e9cb8908cba2cfe3
alazare1 <agefuye@gmail.com>
2023-04-03 04:06:30 -0700

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	int n = 0;
+
+	while(n >= 0 && n < 10)
+	{
+		printf("%d\n",n);
+		n++;
+	}
+
+	return (0);
+}
