749e6c223408de41c1bdbcd4b5da80ab22798487
alazare1 <agefuye@gmail.com>
2023-04-03 04:55:16 -0700

    print number 0-9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int index = 0;
+
+	while (n >= 0 && n < 10)
+	{
+		putchar(index + '0');
+		index++;
+	}
+	putchar("/n");
+
+	return (0);
+}
