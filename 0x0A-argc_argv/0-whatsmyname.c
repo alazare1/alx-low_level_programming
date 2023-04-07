37e2889eb6ceee40cafe306da08c15854cfa2504
alazare1 <agefuye@gmail.com>
2023-04-07 15:53:13 -0700

    print name argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: counts arguments
+ * @argv: array for strings
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
