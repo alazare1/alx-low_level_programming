3f6ee850fd90353691cce33f195ee00e6d0afdd9
alazar <alazar@AXL-AlazarEfuye-2023-2.myguest.virtualbox.org>
2023-06-08 01:51:25 +0300

    fixed

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * _strlen_recursion - gets length of a string
  * @s: Pointer to the start of the string
@@ -7,12 +9,12 @@
 
 int _strlen_recursion(char *s)
 {
-    if (*s)
-    {
-        return (1 + _strlen_recursion(s + 1));
-    }
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
     else
     {
-        return (0);
+		return (0);
     }
 }
