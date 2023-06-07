18ae31632c9af1108a23cea6809b5862f9ab5e00
alazar <alazar@AXL-AlazarEfuye-2023-2.myguest.virtualbox.org>
2023-06-08 01:42:28 +0300

    try errouneous check

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,3 +1,10 @@
+/**
+ * _strlen_recursion - gets length of a string
+ * @s: Pointer to the start of the string
+ *
+ * Return: int corresponding to string length
+ */
+
 int _strlen_recursion(char *s)
 {
     if (*s)
