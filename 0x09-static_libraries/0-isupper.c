39845464f182fe0ce58b53fc6fb4974484ed5005
alazar <alazar@AXL-AlazarEfuye-2023-2.myguest.virtualbox.org>
2023-06-09 23:53:40 +0300

    test run 3

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: the character to check
+ *
+ * Return: 0 if lower case and 1 if upper case 
+ */
+int _isupper(int c)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ *
+ * Return: The length of the string.
+ */
+
+
+
+
+
+int _strlen(char *s)
+{
+	char *p = s;
+
+	while (*p)
+	{
+	p++;
+	}
+	return (p - s);
+}
