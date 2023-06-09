5d61a0c19b373902d86bf363d2c80f52c0955883
alazar <alazar@AXL-AlazarEfuye-2023-2.myguest.virtualbox.org>
2023-06-09 23:44:47 +0300

    test run 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,14 +10,5 @@ int main(int argc, char *argv[])
 {
 	char * program_name = argv[0];
 	printf("%s\n", program_name);
-
-	// while (*program_name)
-	// {
-	// 	_putchar(program_name);
-
-	// 	program_name += 1;
-	// }
-
-	// _putchar('\n');
 	return 0;
 }
