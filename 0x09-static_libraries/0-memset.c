16bbcb84603da2fb958620c139f6f11d01f8f337
alazar <alazar@AXL-AlazarEfuye-2023-2.myguest.virtualbox.org>
2023-06-11 18:32:23 +0300

    try3

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,3 +1,12 @@
+/**
+ * _memset - checks if a character is uppercase
+ * @s: the character to check
+ * @b: the character to check
+ * @n: the character to check
+ *
+ * Return: 0 if lower case and 1 if upper case 
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
     return s;
