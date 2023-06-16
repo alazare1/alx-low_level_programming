47878ae3ae9ad11b5daf879c68427e0c0e9202e7
alazar <alazar@AXL-AlazarEfuye-2023-2.myguest.virtualbox.org>
2023-06-17 02:13:28 +0300

    final9

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -40,6 +40,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		free(ptr);
 		return (NULL);
 	}
+    else if (old_size == new_size)
+    {
+        return ptr;
+    }
 	else
 	{
 		unsigned int min = get_min(old_size, new_size);
